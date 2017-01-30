6451cea01524e54a3032a7f2960bcd44cc8e3dc6
Randy Rankin <randyrankin@users.noreply.github.com>
2017-01-30 13:19:18 -0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,17 +79,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Can you see me now?
-
-                    here is another update.
+                    dev.txkug.com
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://www.txkug.com">Visit the current site</a>
                 </div>
             </div>
         </div>
