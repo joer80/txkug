807540598d36793850995329bbaab3a9353827ff
Christopher Byrd <christopher.byrd2013@gmail.com>
2017-01-30 10:04:11 -0600

    Added change to welcome view.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Can you see me?
                 </div>
 
                 <div class="links">
