b7e04254cb3656bdb73a05060720e07227672f8a
Randy Rankin <rrrankin@gmail.com>
2017-02-03 14:11:46 -0600

    Corrected welcome css issue

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -14,6 +14,39 @@
                 </li>
             </ul>
 
+
+            <ul class="navbar-nav">
+                @if (Auth::guest())
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ env('SLACK_REDIRECT') }}">Slack Login</a>
+                    </li>
+                @else
+                    @if(Auth::user()->hasRole('administrator'))
+
+                        <li class="nav-item btn-group">
+                            <a class="nav-link dropdown-toggle" id="adminDropMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Admin</a>
+                            <div class="dropdown-menu" aria-labelledby="adminDropMenu">
+                                <a class="dropdown-item" href="/admin">Dashboard</a>
+                                <a class="dropdown-item" href="/admin/venues">Venues</a>
+                                <a class="dropdown-item" href="/admin/events">Events</a>
+                                <a class="dropdown-item" href="/admin/users">Users</a>
+                            </div>
+                        </li>
+
+                    @endif
+
+                    <li class="nav-item btn-group">
+                        <a class="nav-link dropdown-toggle" id="userDropMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</a>
+                        <div class="dropdown-menu" aria-labelledby="userDropMenu">
+                            <a class="dropdown-item" href="{{ url('/user/meetings') }}">My Events</a>
+                            <a class="dropdown-item" href="{{ url('/logout') }}">Logout</a>
+                        </div>
+                    </li>
+
+                @endif
+
+            </ul>
+
         </div>
     </div>
 </nav>
